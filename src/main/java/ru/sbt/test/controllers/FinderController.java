defa302121a26c7d304a55a51f5e7c12a4d9eaca
Denis Bakonin <programmernan@mail.ru>
2021-03-25 22:25:00 +0300

    Revert "file uploaded changes"
    This reverts commit 1e0f80c2bf6455e599ac438c45d8d57baf77b4fa.

--- src/main/java/ru/sbt/test/controllers/FinderController.java
+++ src/main/java/ru/sbt/test/controllers/FinderController.java
@@ -3,6 +3,7 @@ package ru.sbt.test.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +11,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.sbt.test.logic.SentenceDictionaryFinder;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.*;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -21,19 +21,12 @@ public class FinderController {
 
     private static final Logger logger = LoggerFactory.getLogger(FinderController.class);
 
-    private static final String UPLOAD_DIR = "/upload/";
-
     private final SentenceDictionaryFinder finder;
 
     private List<String> sentences;
 
-    private HttpServletRequest request;
-
-    private File inputFile;
-
-    public FinderController(SentenceDictionaryFinder finder, HttpServletRequest request) {
+    public FinderController(SentenceDictionaryFinder finder) {
         this.finder = finder;
-        this.request = request;
     }
 
     @GetMapping("/")
@@ -47,13 +40,8 @@ public class FinderController {
         logger.info("post file request!");
         if (file != null) {
             if (!file.isEmpty()) {
-                String uploadDirRealPath = request.getServletContext().getRealPath(UPLOAD_DIR);
-                if (! new File(uploadDirRealPath).exists()) {
-                    new File(uploadDirRealPath).mkdir();
-                }
-                this.inputFile = new File(uploadDirRealPath + file.getOriginalFilename());
-                logger.info(uploadDirRealPath + file.getOriginalFilename());
-                file.transferTo(this.inputFile);
+                String fileString = new String(file.getBytes(), "UTF-8");
+                this.sentences = Arrays.asList(fileString.split("\r\n"));
                 ra.addFlashAttribute("dictSuccess", "Словарь успешно загружен!");
             } else {
                 logger.info("dictStatus - empty");
@@ -67,30 +55,15 @@ public class FinderController {
     }
 
     @PostMapping("/")
-    public String handleLineRelevant(@RequestParam("line") String line, RedirectAttributes ra) throws IOException {
+    public String handleLineRelevant(@RequestParam("line") String line, RedirectAttributes ra) {
         logger.info("post line request!");
 
-        if (this.inputFile != null && this.inputFile.exists()) {
-            this.sentences = new ArrayList<>();
-
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(this.inputFile));
-            String sentence = bufferedReader.readLine();
-            while (sentence != null) {
-                this.sentences.add(sentence);
-                sentence = bufferedReader.readLine();
-            }
-
-            Map<String, Integer> relevantSentences = this.finder.findRelevantSentences(line, this.sentences);
+        if (this.sentences != null && !this.sentences.isEmpty()) {
+            Map<String, Integer> relevantSentences = finder.findRelevantSentences(line, this.sentences);
             ra.addFlashAttribute("relevantSentences", relevantSentences);
             ra.addFlashAttribute("userLine", line);
-
-            if (this.inputFile.delete()) {
-                logger.info("Словарь удален!");
-            } else {
-                logger.info("Словарь не удален!");
-            }
         } else {
-            ra.addFlashAttribute("dictionaryError", "Загрузите словарь для выполнения операции!");
+            ra.addFlashAttribute("dictionaryError", "Словарь не загружен! Загрузите словарь для начала поиска!");
         }
 
         return "redirect:/";
